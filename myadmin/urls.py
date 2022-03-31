492e3784601327e7561d8571a3cfd58adb63b6d2
= <=>
2022-03-30 21:15:19 -0400

    Add list, create, and update views for users

--- myadmin/urls.py
+++ myadmin/urls.py
@@ -4,5 +4,9 @@ from . import views
 
 urlpatterns = [
 	path('', views.dashboard, name='dashboard'),
-	path('profile/<str:pk>/', views.userProfile, name='profile')
+	path('profile/<str:pk>/', views.userProfile, name='profile'),
+
+	path('users/', views.userList, name='user-list'),
+	path('users/create/', views.createUser, name='create-user'),
+	path('users/update/<str:pk>/', views.updateUser, name='update-user'),
 ]
--- myadmin/views.py
+++ myadmin/views.py
@@ -1,3 +1,4 @@
+from django.contrib import messages
 from django.contrib.auth import get_user_model
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import redirect, render
@@ -5,10 +6,12 @@ from django.shortcuts import redirect, render
 User = get_user_model()
 
 from users.forms import UserForm
+from users.models import Role
 
 @login_required(login_url='login')
 def dashboard(request):
 	return render(request, 'myadmin/dashboard.html')
+
 
 @login_required(login_url='login')
 def userProfile(request, pk):
@@ -27,3 +30,79 @@ def userProfile(request, pk):
 	}
 
 	return render(request, 'myadmin/profile.html', context)
+
+
+@login_required(login_url='login')
+def userList(request):
+	users = User.objects.all()
+
+	return render(request, 'myadmin/user_list.html', {'users': users})
+
+
+@login_required(login_url='login')
+def createUser(request):
+	form = UserForm()
+
+	if request.method == 'POST':
+		data = request.POST
+		print(data)
+		image = request.FILES.get('avatar')
+
+		password = User.objects.make_random_password()
+		print(password)
+
+		user = User.objects.create_user(
+			username=data['username'],
+			email=data['email'],
+			password=password
+		)
+		user.avatar = image
+		user.first_name = data['first_name']
+		user.last_name = data['last_name']
+		if data['role']:
+			user.role = Role.objects.get(id=data['role'])
+		user.phone_number = data['phone_number']
+
+		user.save()
+
+		# Check if the other fields are present:
+		# do something like for field in data:
+		# 	if not username/email and exists then update the user
+
+		return redirect('dashboard')
+
+	return render(request, 'myadmin/user_form.html', {'form': form})
+
+	# johny: FGc6SpUpdg
+	# kim: FaaxqumXua
+	# dp: qPM9HRFtfa
+	# ac: t43P93SnzA
+
+@login_required(login_url='login')
+def updateUser(request, pk):
+	user = User.objects.get(id=pk)
+	form = UserForm(instance=user)
+
+	if request.method == 'POST':
+		form = UserForm(request.POST, request.FILES, instance=user)
+		if form.is_valid():
+			form.save()
+			return redirect('dashboard')
+		else:
+			messages.error(request, form.errors)
+
+	context = {
+		'form': form,
+		'edit_user': user,
+	}
+
+	return render(request, 'myadmin/user_form.html', context)
+
+
+@login_required(login_url='login')
+def deleteUser(request, pk):
+	user = User.objects.get(id=pk)
+
+	if request.method == 'POST':
+		user.delete()
+		return redirect('user-list')
